acb1f64969fdfe4b686aa88db1e51de13d40324c
karthisgk <karthisg.sg@gmail.com>
2021-02-06 09:53:52 +0530

    finished

--- Pattern.java
+++ Pattern.java
@@ -3,8 +3,16 @@ class Pattern {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
         int num = inp.nextInt();
-        for (int i = num; i >= 0; i--){
-            System.out.println(i);
+        for (int i = num; i >= 1; i--){
+            int k = 1;
+            for (int j = num; j >= 1; j--){
+                if(i >= j)
+                    System.out.print(i + " ");
+                else
+                    System.out.print((i+k) + " ");
+            }
+            k++;
+            System.out.println();
         }
     }
 }
